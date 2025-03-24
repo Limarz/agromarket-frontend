e19134b4f49cf9ed856138889d16772e560ddb9e
Limar <alexup42@gmail.com>
2025-03-24 12:07:18 +0300

    Fix background image stretching and improve Home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Добро пожаловать в AgroMarket</h1>
       <p>Здесь вы можете купить свежие продукты прямо от фермеров.</p>
     </div>
